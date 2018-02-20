061f409b99b37e5ffad6da449c5a941c1fad6294
jehoopz92 <30909814+jehoopz92@users.noreply.github.com>
2018-02-19 22:23:37 -0500

    final contact form design

--- resources/JS/contact.js
+++ resources/JS/contact.js
@@ -1,3 +1,8 @@
-function webdev() {
-    var popup = document.getElementById("contactForm").style.display = "block";
+function email() {
+    var name = document.getElementById('name').value;
+    var company = document.getElementById('company').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
+    
+    window.location =('mailto:jehoopz92@gmail.com;chade2540@gmail.com?Subject=I would like some information&body= Hellow my name is '+name+'. '+message+'. Below is my contact information. Company Name:'+company+', Phone Number:'+phone+'');
 }
