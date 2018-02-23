626c377b10186de25df31f84965f4acfd8a185ed
jehoopz92 <30909814+jehoopz92@users.noreply.github.com>
2018-02-23 14:02:51 -0500

    test

--- resources/JS/contact.js
+++ resources/JS/contact.js
@@ -4,5 +4,5 @@ function email() {
     var phone = document.getElementById('phone').value;
     var message = document.getElementById('message').value;
     
-    window.location =('mailto:jehoopz92@gmail.com;chade2540@gmail.com?Subject=I would like some information&body= Hello my name is '+name+'. '+message+'. Below is my contact information. Company Name:'+company+', Phone Number:'+phone+'');
+    window.location = ('mailto:jehoopz92@gmail.com;chade2540@gmail.com?Subject=I would like some information&body= Hello my name is '+name+'. '+message+'. Below is my contact information. Company Name:'+company+', Phone Number:'+phone+'');
 }
