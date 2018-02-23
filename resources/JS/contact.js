a34bdcd4c28d0130b5f9d147c5b9cd9f7a8bea17
jehoopz92 <30909814+jehoopz92@users.noreply.github.com>
2018-02-23 17:47:03 -0500

    test

--- resources/JS/contact.js
+++ resources/JS/contact.js
@@ -1,7 +1,8 @@
-function exportMe() {
-    var trucks = document.getElementById('trucks').value;
-    var stops = document.getElementById('stops').value;
-    var piece = document.getElementById('piece').value;
+function email() {
+    var trucks = document.getElementById('name').value;
+    var stops = document.getElementById('company').value;
+    var piece = document.getElementById('phone').value;
+    var piece = document.getElementById('message').value;
     window.location = ('mailto:lberuashvili@uste3.com;dpatterson@uste3.com;mkrause@uste3.com;mdurphy@uste3.com;cmuniz@uste3.com;emmett.gilbert@conns.com;Aniouka.Cuba@conns.com;terry.johnston@conns.com;alan.lovett@conns.com;monique.lavan@conns.com;henry.milton@conns.com;jhurley@uste3.com;aleplatt@uste3.com;jemery@uste3.com;spowell@uste3.com?Subject=Conns Houston Export for  with an average of &body=Houston has been exported with ' + trucks + ' trucks, ' + stops + ' stops and ' + piece + ' pieces. Please refresh DT and let me know when drivers are assigned, thank you!');
 }
 
