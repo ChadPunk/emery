1b036476fe7601df9b71034b3d4aef764b7c6ee0
jehoopz92 <30909814+jehoopz92@users.noreply.github.com>
2018-02-25 14:59:17 -0500

    final test

--- resources/JS/contact.js
+++ resources/JS/contact.js
@@ -1,8 +1,9 @@
 function exportme() {
-    var trucks = document.getElementById('name').value;
-    var stops = document.getElementById('company').value;
-    var piece = document.getElementById('phone').value;
+    var name = document.getElementById('name').value;
+    var company = document.getElementById('company').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
    
-    window.location = ('mailto:lberuashvili@uste3.com;dpatterson@uste3.com;mkrause@uste3.com;mdurphy@uste3.com;cmuniz@uste3.com;emmett.gilbert@conns.com;Aniouka.Cuba@conns.com;terry.johnston@conns.com;alan.lovett@conns.com;monique.lavan@conns.com;henry.milton@conns.com;jhurley@uste3.com;aleplatt@uste3.com;jemery@uste3.com;spowell@uste3.com?Subject=Conns Houston Export for  with an average of &body=Houston has been exported with ' + name + ' trucks, ' + company + ' stops and ' + phone + ' pieces. Please refresh DT and let me know when drivers are assigned, thank you!');
+    window.location = ('mailto:chade2540@gmail.com; jehoopz92@gmail.com?Subject=I would like some info&body=Hello, my name is '+name+'. '+message+'. Company name: '+company+'.  Phone Number: '+phone+' ');
 }
 
