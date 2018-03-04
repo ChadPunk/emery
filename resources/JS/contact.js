455efd0d730cbe73fc20acef179dab1acaa19e56
jehoopz92 <30909814+jehoopz92@users.noreply.github.com>
2018-03-03 22:26:51 -0500

    Changes

--- resources/JS/contact.js
+++
@@ -1,9 +0,0 @@
-function exportme() {
-    var name = document.getElementById('name').value;
-    var company = document.getElementById('company').value;
-    var phone = document.getElementById('phone').value;
-    var message = document.getElementById('message').value;
-   
-    window.location = ('mailto:chade2540@gmail.com; jehoopz92@gmail.com?Subject=I would like some info&body=Hello, my name is '+name+'. '+message+'. Company name: '+company+'.  Phone Number: '+phone+' ');
-}
-
