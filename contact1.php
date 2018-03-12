12b7a104e4b827517d65bf32c9acde6c7486f266
jehoopz92 <30909814+jehoopz92@users.noreply.github.com>
2018-03-11 22:28:16 -0400

    final design

--- contact1.php
+++ contact1.php
@@ -1,35 +1,23 @@
 <?php
-/*
- *  CONFIGURE EVERYTHING HERE
- */
 
-// an email address that will be in the From field of the email.
-$from = 'Demo contact form <admin@opendoor-ff.com>';
+$from = $_POST['email'];
 
-// an email address that will receive the email with the output of the form
+
 $sendTo = 'jon@emerywd.com';
 
-// subject of the email
+
 $subject = 'New message from contact form';
 
-// form field names and their translations.
-// array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'checkbox1' => 'Logo');
+
+$fields = array('name' => 'Name', 'surname' => 'Last Name', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message', 'checkbox1' => 'What I am looking for','checkbox2' => 'What I am looking for','checkbox3' => 'What I am looking for','checkbox4' => 'What I am looking for','checkbox5' => 'Do you have a site','checkbox6' => 'Do you have a site', 'link' => 'Link', 'checkbox7' => 'The website i am looking for is','checkbox8' => 'The website i am looking for is','checkbox9' => 'The website i am looking for is','checkbox10' => 'The website i am looking for is','checkbox11' => 'The website i am looking for is','checkbox12' => 'The website i am looking for is','checkbox13' => 'I am looking for this feature','checkbox14' => 'I am looking for this feature','checkbox15' => 'I am looking for this feature','checkbox16' => 'I am looking for this feature','checkbox17' => 'I am looking for this feature','checkbox18' => 'Web hosting','checkbox19' => 'Web hosting','checkbox20' => 'Web hosting','checkbox21' => 'Do you have a budget','checkbox22' => 'Do you have a budget', 'link1' => 'Our budget is');
 
 
-
-
-// message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 
-// If something goes wrong, we will display this message.
+
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
-/*
- *  LET'S DO THE SENDING
- */
 
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(E_ALL & ~E_NOTICE);
 
 try
@@ -40,20 +28,20 @@ try
     $emailText = "You have a new message from your contact form\n=============================\n";
 
     foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email
+        
         if (isset($fields[$key])) {
             $emailText .= "$fields[$key]: $value\n";
         }
     }
 
-    // All the neccessary headers for the email.
+    
     $headers = array('Content-Type: text/plain; charset="UTF-8";',
         'From: ' . $from,
         'Reply-To: ' . $from,
         'Return-Path: ' . $from,
     );
 
-    // Send email
+    
     mail($sendTo, $subject, $emailText, implode("\n", $headers));
 
     $responseArray = array('type' => 'success', 'message' => $okMessage);
@@ -65,11 +53,11 @@ catch (\Exception $e)
 
 
 if ($responseArray['type'] == 'success') {
-    // success redirect
+   
 
     header('Location: /success');
 }
 else {
-    //error redirect
+    
     header('Location: /failed');
 }
